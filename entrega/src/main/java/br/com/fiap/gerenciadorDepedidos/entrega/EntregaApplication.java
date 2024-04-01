23866664019123fd9dda2e94ea6a5a1b639f8f59
matheushajer <maatheushajer@gmail.com>
2024-04-01 17:36:36 -0300

    Configuração do server e gateway, para gerenciar os microservices e configuração base dos bancos.s

--- entrega/src/main/java/br/com/fiap/gerenciadorDepedidos/entrega/EntregaApplication.java
+++ entrega/src/main/java/br/com/fiap/gerenciadorDepedidos/entrega/EntregaApplication.java
@@ -2,8 +2,10 @@ package br.com.fiap.gerenciadorDepedidos.entrega;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EntregaApplication {
 
 	public static void main(String[] args) {
--- pedidos/src/main/java/br/com/fiap/gerenciadorDepedidos/pedidos/PedidosApplication.java
+++ pedidos/src/main/java/br/com/fiap/gerenciadorDepedidos/pedidos/PedidosApplication.java
@@ -2,8 +2,12 @@ package br.com.fiap.gerenciadorDepedidos.pedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class PedidosApplication {
 
 	public static void main(String[] args) {
