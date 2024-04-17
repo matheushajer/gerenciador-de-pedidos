b8bad11add3fa1491270cb4b878aae098bebf131
Karoline Leite <RM351684@fiap.com.br>
2024-04-17 13:39:29 -0300

    add use case atualizar endereco principal

--- clientes/src/main/java/br/com/fiap/gerenciadorDepedidos/clientes/repositories/EnderecoRepository.java
+++ clientes/src/main/java/br/com/fiap/gerenciadorDepedidos/clientes/repositories/EnderecoRepository.java
@@ -4,4 +4,5 @@ import br.com.fiap.gerenciadorDepedidos.clientes.entities.EnderecoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EnderecoRepository extends JpaRepository<EnderecoEntity, Long> {
+    EnderecoEntity findByClienteEntityIdAndIsEnderecoPrincipalIsTrue(Long clienteId);
 }
