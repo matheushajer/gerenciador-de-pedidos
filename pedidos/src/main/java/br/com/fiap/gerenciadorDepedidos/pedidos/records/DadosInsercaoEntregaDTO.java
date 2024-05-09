1fcc9270ad20646d00d72cf680a205d66f72ea86
matheushajer <maatheushajer@gmail.com>
2024-05-09 16:54:50 -0300

    Ajuste na validação do codigoDeRastreio dentro do DTO DadosInsercaoEntregaDTO

--- pedidos/src/main/java/br/com/fiap/gerenciadorDepedidos/pedidos/records/DadosInsercaoEntregaDTO.java
+++ pedidos/src/main/java/br/com/fiap/gerenciadorDepedidos/pedidos/records/DadosInsercaoEntregaDTO.java
@@ -1,8 +1,8 @@
 package br.com.fiap.gerenciadorDepedidos.pedidos.records;
 
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
 
@@ -15,7 +15,7 @@ public record DadosInsercaoEntregaDTO(
         @Min(value = 0, message = "O valor do frete deve ser maior ou igual a zero.")
         BigDecimal frete,
 
-        @Size(min = 8, max = 8, message = "O Código de Rastreio deve conter oito digitos")
+        @Digits(integer = 8, fraction = 0, message = "O Código de Rastreio deve conter oito digitos")
         @NotNull(message = "O código de rastreio é obrigatório e deve ser um número de 8 dígitos.")
         Integer codigoDeRastreio
 ) {
